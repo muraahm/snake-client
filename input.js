86b440fab66bc5abc2b46b1a095c31da9edd4175
muraahm <ahmedmuraahm121@gmail.com>
2019-07-31 23:09:38 +0000

    updated input

--- input.js
+++ input.js
@@ -1,10 +1,27 @@
+const { connect } = require('./client');
+let connection;
+
 const handleUserInput = function (data) {
 
+  if (data === "w") {
+    connection.write("Move: up")
+  }
+  if (data === "a") {
+    connection.write("Move: left")
+  }
+  if (data === "d") {
+    connection.write("Move: right")
+  }
+  if (data === "s") {
+    connection.write("Move: down")
+  }
   if (data === '\u0003') {
     process.exit();
   }
 }
-const setupInput = function () {
+const setupInput = function (conn) {
+  connection = conn;
+
   const stdin = process.stdin;
   stdin.on('data', handleUserInput)
   stdin.setRawMode(true);
