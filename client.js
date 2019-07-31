971d9f9d9aad16ca553659d16879529bfb6fee50
muraahm <ahmedmuraahm121@gmail.com>
2019-07-31 20:22:19 +0000

    updated client

--- client.js
+++ client.js
@@ -2,20 +2,25 @@ const net = require('net');
 /**
  * Establishes connection with the game server
  */
-const connect = function() {
-  const conn = net.createConnection({ 
+const connect = function () {
+  const conn = net.createConnection({
     host: '10.0.2.15',
     port: 50541
   });
-  conn.on('connect', function() {
+  conn.on('connect', function () {
     console.log('Successfully connected to game server')
     conn.write("Name: AHM")
-  }) 
-  conn.on('data', function(message) {
+    // conn.write("Move: up")
+    // setTimeout(() => {
+    //   conn.write("Move: up");
+    // }, 50);
+  })
+  conn.on('data', function (message) {
     console.log('From Server', message)
-  }) 
-  conn.setEncoding('utf8'); 
-  
+  })
+
+  conn.setEncoding('utf8');
+
   return conn;
 }
-module.exports = {connect}
+module.exports = { connect }
